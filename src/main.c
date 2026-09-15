9b680b343805a4188bee1e86416efc61e7177a72
Gu1Assis <assis.guisilva@gmail.com>
2026-09-14 23:50:28 -0300

    add command interface

--- src/main.c
+++ src/main.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-  printf("Wellcome to task! Please come back later WIP!!!\n");
+int main(int argc, char *argv[]) {
+  
+  if(argc==1 || strcmp(argv[1],"--help")==0){
+    printf("Welcome to task!\n");
+    return 0;
+  }
+
+  if(strcmp(argv[1], "add")==0){
+    printf("Input data to add new task!\n");
+    return 0;
+  }
+
+  if(strcmp(argv[1], "ls")==0){
+    printf("Here is your data!\n");
+    return 0;
+  }
+
+  if(strcmp(argv[1], "complete")==0){
+    printf("Input data to add complete task!\n");
+    return 0;
+  }
+
+  if(strcmp(argv[1], "cancel")==0){
+    printf("Input data to cancel task!\n");
+    return 0;
+  }
+
+  if(strcmp(argv[1], "sync")==0){
+    printf("Syncing data...\n");
+    return 0;
+  }
+
+  printf("Command not found! Try using task --help");
   return 0;
 }
